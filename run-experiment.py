2b2040cdeb374edee4627b4f3c45a70cfe1926b5
drammock <dan.mccloy@gmail.com>
2016-03-04 13:58:07 -0800

    runs, but video laggy

--- run-experiment.py
+++ run-experiment.py
@@ -20,7 +20,7 @@ import os.path as op
 # load experiment parameters
 globalvars = np.load(op.join('params', 'global-params.npz'))
 wav_array = globalvars['wav_array']
-wav_nsamps = globalvars['wav_nsamps']
+wav_nsamp = globalvars['wav_nsamps']
 wav_names = globalvars['wavnames'].tolist()
 fs = ef.stimuli.get_tdt_rates()['25k'] if round(globalvars['fs']) == 24414 \
     else globalvars['fs']
@@ -28,6 +28,8 @@ del globalvars
 # load & calc trial-level params
 df = pd.read_csv(op.join('params', 'master-dataframe.tsv'), sep='\t')
 df['wav_path'] = df['talker'] + op.sep + df['syll'] + '.wav'
+df['wav_idx'] = [wav_names.index(x) for x in df['wav_path']]
+df['wav_nsamp'] = [wav_nsamp[x] for x in df['wav_idx']]
 df['onset_sec'] = df['onset'] / fs
 df['offset_sec'] = df['offset'] / fs
 df['trial_id'] = (df['block'].astype(int).apply(format, args=('02',)) + '_' +
@@ -41,6 +43,7 @@ ec_args = dict(exp_name='jsalt-follow-up', full_screen=True, enable_video=True,
                participant='foo', session='1', version='dev', stim_rms=0.02,
                stim_db=70.)
 with ef.ExperimentController(**ec_args) as ec:
+    screen_period = 1. / ec.estimate_screen_fs(60)
     subj = int(ec.session)
     audio = sorted(glob(op.join('stimuli-final', 'subj-{:02}'.format(subj),
                                 '*.wav')))
@@ -49,11 +52,6 @@ with ef.ExperimentController(**ec_args) as ec:
     assert blocks in (12, 13)
     # reduce data frame to subject-specific data
     df = df[df['subj'].isin([subj])].reset_index(drop=True)
-    '''
-    df['ttl_id'] = (df['subj'].astype(int).apply(format, args=('02',)) +
-                    df['block'].astype(int).apply(format, args=('02',)) +
-                    df.index.to_series().apply(format, args=('03',)))
-    '''
     for block in range(blocks):
         # subset data frame for this block
         this_df = df[df['block'].isin([block])].copy()
@@ -68,39 +66,45 @@ with ef.ExperimentController(**ec_args) as ec:
         vpath = op.join('videos', vname)
         assert vpath in video
         ec.load_video(vpath)
+        ec.video.set_scale(fill=True)
         ec.call_on_next_flip(ec.video.play)
-        # partially construct the ttl id
-        ttl_id = np.binary_repr(subj, 4) + np.binary_repr(block, 4)
+        # construct the full ttl id
+        ttl_base = np.binary_repr(subj, 4) + np.binary_repr(block, 4)
+        this_df['ttl_id'] = this_df['ttl_id'].apply(lambda x: ttl_base + x)
+        this_df['ttl_id'] = this_df['ttl_id'].apply(lambda x:
+                                                    [int(y) for y in list(x)])
         # iterate through syllables
-        for ix, (wav_path, onset, offset, trial_id,
-                 ttl_id) in enumerate(zip(this_df['wav_path'].values,
-                                          this_df['next_onset_sec'].values,
-                                          this_df['offset_sec'].values,
-                                          this_df['trial_id'].values,
-                                          this_df['ttl_id'].values)):
-            wav_idx = wav_names.index(wav_path)
-            wav = wav_array[wav_idx, :, :wav_nsamps[wav_idx]]
-            ec.load_buffer(wav)
-            ttl_id = [int(x) for x in list(ttl_id)]
+        strings = this_df['trial_id'].values.astype(str)
+        floats = this_df[['onset_sec', 'offset_sec']].values
+        ints = this_df[['wav_idx', 'wav_nsamp']].values
+        objs = this_df['ttl_id'].values
+        ec.load_buffer(np.zeros((2, 24414)))
+        for ix, (trial_id, (onset, offset), (wav_idx, wav_nsamp),
+                 ttl_id) in enumerate(zip(strings, floats, ints, objs)):
+            #wav = wav_array[wav_idx, :, :wav_nsamp]
+            #ec.load_buffer(wav)
             ec.identify_trial(ec_id=trial_id, ttl_id=ttl_id)
-            # initial start
+            # start initial stimulus
             if not ix:
+                t = ec.flip()
                 t_zero = ec.start_stimulus()
             # timing
-            t = ec.get_time()
-            next_audio_start = t_zero + onset
+            this_audio_start = t_zero + onset - screen_period / 2
             this_audio_stop = t_zero + offset
-            next_frame_time = (ec.video.last_timestamp + ec.video.dt +
-                               ec.video.time_offset)
-            while t < next_audio_start:
-                t = ec.get_time()
-                if t > this_audio_stop:
-                    ec.stop()
-                    ec.trial_ok()
-                ec.flip(when=next_frame_time)
-            continue
-            '''
             t = ec.get_time()
-            flip_video = next_frame_time < t
-            ec.start_stimulus(flip=flip_video)
-            '''
+            if ix:
+                while t < this_audio_start:
+                    t = ec.flip()
+                    #flips.append(t)
+                t = ec.start_stimulus()
+            while t < this_audio_stop:
+                t = ec.flip()
+                #flips.append(t)
+            ec.stop()
+            ec.trial_ok()
+            #ec.flush()
+
+'''
+from matplotlib import pyplot as plt
+_ = plt.hist(np.diff(flips), bins=100)
+'''
