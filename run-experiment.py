f63f4825ddbc82d910efb515418620695b4843e4
drammock <dan.mccloy@gmail.com>
2016-03-22 09:56:31 -0700

    video playback through vlc

--- run-experiment.py
+++ run-experiment.py
@@ -15,6 +15,8 @@ import numpy as np
 from pandas import read_csv
 from expyfun import ExperimentController
 from expyfun.stimuli import get_tdt_rates
+from platform import system
+from subprocess import call
 from glob import glob
 import os.path as op
 
@@ -43,14 +45,20 @@ df['ttl_id'] = df['ttl_id'].apply(lambda x: [int(y) for y in list(x)])
 video = sorted(glob(op.join('videos', '*.m4v')))
 assert len(video) == 20
 
+# instructions
+instructions = ('In this experiment you will hear speech sounds from several '
+                'different talkers. All you have to do is passively listen. '
+                'To prevent boredom, a silent cartoon will play during each '
+                'block. Press the 1 button when you\'re ready to start.')
+
 # startup ExperimentController
 continue_key = 1
-ec_args = dict(exp_name='jsalt-follow-up', full_screen=True, enable_video=True,
-               participant='foo', session='0', version='dev',  # 0ee0951 (drammock, 6a74a7c)
+ec_args = dict(exp_name='jsalt-follow-up', full_screen=True,
+               participant='pilot', session='5', version='0ee0951',
                stim_rms=0.01, stim_db=65., output_dir='expyfun-data-raw')
 
 with ExperimentController(**ec_args) as ec:
-    screen_period = 1. / ec.estimate_screen_fs(60)
+    ec.screen_prompt(instructions)
     subj = int(ec.session)
     audio = sorted(glob(op.join('stimuli-final', 'subj-{:02}'.format(subj),
                                 '*.wav')))
@@ -60,6 +68,8 @@ with ExperimentController(**ec_args) as ec:
     # reduce data frame to subject-specific data
     subj_df = df[df['subj'].isin([subj])].reset_index(drop=True)
     for block in range(blocks):
+        ec.screen_prompt('Here we go!', max_wait=0.7, live_keys=[], attr=False)
+        ec.set_visible(False)
         # subset data frame for this block
         blk_df = subj_df[subj_df['block'].isin([block])].copy()
         blk_len = df[['subj', 'block']].groupby('block').aggregate(np.size)
@@ -68,9 +78,11 @@ with ExperimentController(**ec_args) as ec:
         vname = blk_df['vname'].values[0]
         vpath = op.join('videos', vname)
         assert vpath in video
-        ec.load_video(vpath)
-        ec.video.set_scale('fit')
-        ec.call_on_next_flip(ec.video.play)
+        if system() == 'Windows':
+            call(['vlc', '-f', '--no-audio', '--play-and-exit', vpath])
+            #call(['wmplayer', vpath, '/fullscreen'])
+        else:
+            call(['vlc', '-f', '--no-audio', '--play-and-exit', vpath])
         # prepare syllable-level variables
         strings = blk_df['trial_id'].values.astype(str)
         floats = blk_df[['onset_sec', 'offset_sec']].values
@@ -85,22 +97,16 @@ with ExperimentController(**ec_args) as ec:
                                                           delay=0.01))
             # start initial stimulus
             if not ix:
-                t_zero = ec.start_stimulus()
-            # timing
+                t_zero = ec.start_stimulus(flip=False)
             this_audio_start = t_zero + onset
             this_audio_stop = t_zero + offset
-            t = ec.get_time()
             if ix:
-                while t < this_audio_start - screen_period:
-                    t = ec.flip()
-                ec.start_stimulus(flip=False)
-            while t < this_audio_stop:
-                t = ec.flip()
+                ec.start_stimulus(flip=False, when=this_audio_start)
+            ec.wait_until(this_audio_stop)
             ec.stop()
             ec.trial_ok()
-        ec.video.set_visible(False, flip=True)
-        ec.delete_video()
         ec.flush()
+        ec.set_visible(True)
         if block == blocks - 1:
             msg = ('All done! Sit tight and we will come disconnect the EEG.')
             max_wait = 5.
