ae822e7fe2437131a43cf075746006712eb4c0a7
drammock <dan.mccloy@gmail.com>
2016-03-30 17:22:14 -0700

    WIP: epoching of raw EEG data

--- clean-eeg.py
+++ clean-eeg.py
@@ -13,36 +13,52 @@ This script analyzes the expyfun log to make sure there were no anomalies.
 
 import mne
 import numpy as np
-from glob import glob
-from os import path as op
 from os import mkdir
+from os import path as op
 from expyfun import binary_to_decimals
 
 # file i/o
 paramdir = 'params'
-paramfile = 'master-dataframe.tsv'
+paramfile = 'global-params.npz'
 eegdir = 'eeg-data-raw'
 outdir = 'eeg-data-clean'
 if not op.isdir(outdir):
     mkdir(outdir)
 
 # params
-subjects = [1]
-fs = 1000.
-event_id = dict()
+subjects = dict(IJ=1)
+eeg_fs = 1000.
+tmin, tmax = (-0.02, 0.7)
+params = np.load(op.join(paramdir, paramfile))
+wav_names = params['wav_names']
+#wav_array = params['wav_array']
+#stim_fs = params['fs']
+del params
 
-for subj in subjects:
+# create event dict
+master_event_id = dict()
+for _id, name in enumerate(wav_names):
+    master_event_id[name] = _id
+
+for subj_code, subj in subjects.iteritems():
     # read raws
-    header = 'jsalt_binaural_cortical_{:03}.vhdr'.format(subj)
+    header = 'jsalt_binaural_cortical_{0}_{1:03}.vhdr'.format(subj_code, subj)
     raw = mne.io.read_raw_brainvision(op.join(eegdir, header),
                                       preload=True, response_trig_shift=None)
+    mne.io.set_eeg_reference(raw, ref_channels=['Ch17'], copy=False)
     raw_events = mne.find_events(raw)
     mne.write_events(op.join(outdir, header[:-5] + '-raw-eve.bak'), raw_events)
     # decode triggers
     stim_start_indices = np.where(raw_events[:, -1] == 1)[0]
-    id_lims = np.c_[np.r_[0, stim_start_indices + 1][:-1], stim_start_indices]
-    assert np.unique(np.diff(id_lims)) == 9  # number of bits in trigger IDs
+    stim_start_indices = stim_start_indices[1:]  # 1st trigger is block number
+    id_lims = np.c_[np.r_[stim_start_indices - 9], stim_start_indices]
     events = raw_events[stim_start_indices]
     for ix, (st, nd) in enumerate(id_lims):
         events[ix, -1] = binary_to_decimals(raw_events[st:nd, -1] // 4 - 1, 9)
+    event_id = {k: v for k, v in master_event_id.items() if v in events[:, -1]}
     mne.write_events(op.join(outdir, header[:-5] + '-eve.txt'), events)
+    # TODO: shift timepoint of stim-start trigger to C-V boundary
+    # TODO: calc tmin (max duration of pre-boundary Cs)
+    # TODO: calc tmax (max duration of post-boundary Vs)
+    # generate epochs
+    epochs = mne.Epochs(raw, events, event_id)
