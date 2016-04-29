6b4abb96f5a8c632957be835bc8c1a7c64376e72
drammock <dan.mccloy@gmail.com>
2016-04-29 16:56:14 -0700

    working data cleaning/basic classification

--- classify-eeg.py
+++ classify-eeg.py
@@ -18,71 +18,150 @@ import mne
 import numpy as np
 # from os import mkdir
 from os import path as op
-# from expyfun import binary_to_decimals
 from pandas import read_csv
-# from sklearn.svm import LinearSVC
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+from sklearn.svm import LinearSVC
 
 rand = np.random.RandomState(seed=0)
+
+# flags
+align = 'v'  # whether epochs are aligned to consonant (c) or vowel (v) onset
+have_dss = True
+use_dss = True
+n_dss_channels = 1
+
+# TODO: try averaging various numbers of tokens (2, 4, 5) prior to training
+# classifier
+# TODO: try decomposing in the time domain (PCA / whiten / time-domain DSS)
 
 # file i/o
 eegdir = 'eeg-data-clean'
 paramdir = 'params'
-# cvfile = 'cv-boundary-times.tsv'
 
 # load global params
 subjects = np.load(op.join(paramdir, 'subjects.npz'))
-# params = np.load(op.join(paramdir, 'global-params.npz'))
-# wav_names = params['wav_names']
-# del params
-df_cols = ['subj', 'talker', 'syll', 'train', 'wav_path', 'wav_idx']
-df_types = dict(subj=int, talker=str, syll=str, train=bool, wav_path=str,
-                wav_idx=int)
+df_cols = ['subj', 'talker', 'syll', 'train', 'wav_idx']
+df_types = dict(subj=int, talker=str, syll=str, train=bool, wav_idx=int)
 df = read_csv(op.join('params', 'master-dataframe.tsv'), sep='\t',
               usecols=df_cols, dtype=df_types)
-df['eng'] = df.talker.apply(lambda x: x[:3] == 'eng')
-df['valid'] = df['eng'] & ~df['train']
-df['test'] = ~df['eng']
-# make sure training, validation, & testing data don't overlap
+df['lang'] = df['talker'].apply(lambda x: x[:3])
+df['valid'] = (df['lang'] == 'eng') & ~df['train']
+df['test'] = ~(df['lang'] == 'eng')
+# make sure every stimulus is either training, validation, or testing
+# and make sure training, validation, & testing don't overlap
+assert df.shape[0] == df['train'].sum() + df['valid'].sum() + df['test'].sum()
 assert np.all(df[['train', 'valid', 'test']].sum(axis=1) == 1)
 training_dict = {idx: bool_ for idx, bool_ in zip(df['wav_idx'], df['train'])}
 validate_dict = {idx: bool_ for idx, bool_ in zip(df['wav_idx'], df['valid'])}
 testing_dict = {idx: bool_ for idx, bool_ in zip(df['wav_idx'], df['test'])}
-"""
-# load C-V transition times
-df = read_csv(cvfile, sep='\t')
-df['key'] = df['talker'] + '/' + df['consonant'] + '.wav'
-df['key'] = df['key'].apply(unicode)
-assert set(wav_names) - set(df['key']) == set([])  # have a time for all keys?
-"""
 
+# construct mapping from event_id to consonant and language (do as loop instead
+# of dict comprehension to make sure everything is one-to-one)
+df['cons'] = df['syll'].apply(lambda x: x[:-2] if x.split('-')[-1] in
+                              ('0', '1', '2') else x)
+cons_dict = dict()
+lang_dict = dict()
+for key, cons, lang in zip(df['wav_idx'], df['cons'], df['lang']):
+    if key in cons_dict.keys() and cons_dict[key] != cons:
+        raise RuntimeError
+    if key in lang_dict.keys() and lang_dict[key] != lang:
+        raise RuntimeError
+    cons_dict[key] = cons
+    lang_dict[key] = lang
+
+# init some global containers
 epochs = []
 events = []
+subjs = []
+cons = []
+
 # read in cleaned EEG data
 for subj_code, subj in subjects.items():
-    basename = '{0:03}-{1}-'.format(int(subj), subj_code)
-    this_epochs = mne.read_epochs(op.join(eegdir, basename + 'epo.fif.gz'),
-                                  preload=True, proj=False)
-    epochs.extend(this_epochs.get_data())
-    events.extend(this_epochs.events[:, -1])
+    basename = '{0:03}-{1}-{2}-aligned-'.format(int(subj), subj_code, align)
+    if have_dss and use_dss:
+        this_data = np.load(op.join(eegdir, basename + 'dssdata.npy'))
+    else:
+        this_epochs = mne.read_epochs(op.join(eegdir, basename + 'epo.fif.gz'),
+                                      preload=True, proj=False, verbose=False)
+        this_data = this_epochs.get_data()
+        del this_epochs
+        if use_dss:
+            dss_mat = np.load(op.join(eegdir, basename + 'dssmat.npy'))
+            this_data = np.einsum('ij,hjk->hik', dss_mat, this_data)
+            del dss_mat
+    # can't use mne.read_events with 0-valued event_ids
+    this_events = np.loadtxt(op.join(eegdir, basename + 'eve.txt'),
+                             dtype=np.float64).astype(int)[:, -1]
+    this_cons = np.array([cons_dict[e] for e in this_events])
+    this_train_mask = np.array([training_dict[e] for e in this_events])
+    this_valid_mask = np.array([validate_dict[e] for e in this_events])
+    this_test_mask = np.array([testing_dict[e] for e in this_events])
+    this_lang = np.array([lang_dict[e] for e in this_events])
+    # add current subj data to global container
+    epochs.extend(this_data)
+    events.extend(this_events)
+    subjs.extend([subj] * this_data.shape[0])
+    cons.extend(this_cons)
+    # concatenate DSS components
+    data_cat = this_data[:, :n_dss_channels, :].reshape(this_data.shape[0], -1)
+    # do LDA
+    lda_classifier = LDA(solver='svd')
+    lda_trained = lda_classifier.fit(X=data_cat[this_train_mask],
+                                     y=this_cons[this_train_mask])
+    # validate
+    english_validation = lda_trained.predict(data_cat[this_valid_mask])
+    # english_prob = lda_trained.predict_proba(data_cat[this_valid_mask])
+    n_correct = np.sum(this_cons[this_valid_mask] == english_validation)
+    print('{}: {} / {} correct (LDA, {})'.format(subj_code, n_correct,
+                                                 this_valid_mask.sum(),
+                                                 'English'))
+    # test
+    foreign_langs = ', '.join(np.unique(this_lang[this_test_mask]))
+    foreign_testing = lda_trained.predict(data_cat[this_test_mask])
+    n_correct = np.sum(this_cons[this_test_mask] == foreign_testing)
+    print('{}: {} / {} correct (LDA, {})'.format(subj_code, n_correct,
+                                                 this_test_mask.sum(),
+                                                 foreign_langs))
+    '''
+    # do SVM
+    svm_classifier = LinearSVC(dual=False, class_weight='balanced',
+                               random_state=rand)
+    svm_trained = svm_classifier.fit(X=data_cat[this_train_mask],
+                                     y=this_cons[this_train_mask])
+    english_validation = svm_trained.predict(data_cat[this_valid_mask])
+    n_correct = np.sum(this_cons[this_valid_mask] == english_validation)
+    print('{}: {} / {} correct (SVM)'.format(subj_code, n_correct,
+                                             this_valid_mask.sum()))
+    '''
+# convert global containers to arrays
 epochs = np.array(epochs)
 events = np.array(events)
-# compute masks for training, validation, and test
-training_idx = np.array([training_dict[event_id] for event_id in events])
-validate_idx = np.array([validate_dict[event_id] for event_id in events])
-testing_idx = np.array([testing_dict[event_id] for event_id in events])
+subjs = np.squeeze(subjs)
+cons = np.array(cons)
+epochs_cat = epochs[:, :n_dss_channels, :].reshape(epochs.shape[0], -1)
+train_mask = np.array([training_dict[e] for e in events])
+valid_mask = np.array([validate_dict[e] for e in events])
+test_mask = np.array([testing_dict[e] for e in events])
 
-# LDA
-lda_classifier = LDA(solver='svd', store_covariance=True)
-lda_classifier.fit(epochs[training_idx], events[training_idx])
-english_validation = lda_classifier.predict(epochs[validate_idx])
-foreign_prediction = lda_classifier.predict(epochs[testing_idx])
+# do across-subject LDA
+lda_classifier = LDA(solver='svd')
+lda_trained = lda_classifier.fit(X=epochs_cat[train_mask], y=cons[train_mask])
+# validate
+english_validation = lda_trained.predict(epochs_cat[valid_mask])
+n_correct = np.sum(cons[valid_mask] == english_validation)
+print('{}: {} / {} correct (LDA, English)'.format('All subjs', n_correct,
+                                                  valid_mask.sum()))
+# test
+foreign_testing = lda_trained.predict(epochs_cat[test_mask])
+foreign_prob = lda_trained.predict_proba(epochs_cat[test_mask])
+n_correct = np.sum(cons[test_mask] == foreign_testing)
+print('{}: {} / {} correct (LDA, {})'.format('All subjs', n_correct,
+                                             test_mask.sum(), 'All languages'))
 
 """
-# SVM (won't work; requires 2-D input)
-svm_classifier = LinearSVC(kernel='linear', class_weight='balanced',
-                           decision_function_shape='ovr', random_state=rand)
-svm_classifier.fit(epochs[training_idx], events[training_idx])
-english_validation = svm_classifier.predict(epochs[validate_idx])
-foreign_prediction = svm_classifier.predict(epochs[testing_idx])
+import matplotlib.pyplot as plt
+for ev in np.unique(this_events)[:1]:
+    ixs = this_events == ev
+    plt.plot(data[ixs, 0, :].T, linewidth=0.5)
+    plt.plot(data[ixs, 0, :].mean(0), linewidth=1.5)
 """
--- clean-eeg.py
+++ clean-eeg.py
@@ -15,16 +15,20 @@ boundary of each stimulus syllable.
 
 from __future__ import division, print_function
 import mne
-from mne_sandbox.preprocessing import dss
 import numpy as np
+from mne_sandbox.preprocessing import dss
 from os import mkdir
 from os import path as op
 from expyfun import binary_to_decimals
 from pandas import read_csv
+from ast import literal_eval
 
 # manually-set params
 subjects = dict(IJ=1, IL=2, FA=3, IM=4, ID=5, CQ=6)
 do_baseline = True
+save_dss_data = True
+save_dss_mat = True
+save_epochs = True
 
 # file i/o
 paramdir = 'params'
@@ -60,6 +64,13 @@ vowel_durs = np.array([wav_dur_dict[key] for key in wav_names]
 vowel_dur_dict = {key: dur for key, dur in zip(wav_names, vowel_durs)}
 assert np.allclose(wav_durs, consonant_durs + vowel_durs)
 
+# load master data frame
+mdf = read_csv(op.join(paramdir, 'master-dataframe.tsv'), sep='\t')
+# because pandas.read_csv(... dtype) argument doesn't work:
+for col in ('subj', 'block', 'onset', 'offset', 'wav_idx', 'wav_nsamp'):
+    mdf[col] = mdf[col].apply(int)
+mdf['ttl_id'] = mdf['ttl_id'].apply(literal_eval)
+
 # we care about first 200ms of brain response
 tmax = wav_durs.max() + 0.2
 # include lots of pre-stim time: allows later shifting to align C-V transition
@@ -88,27 +99,41 @@ for subj_code, subj in subjects.items():
         h = 'jsalt_binaural_cortical_{0}_{1:03}-2.vhdr'.format(subj_code, subj)
         raw2 = mne.io.read_raw_brainvision(op.join(eegdir, h), preload=True,
                                            response_trig_shift=None)
+        two_runs = True
+    except IOError:
+        two_runs = False
+    if two_runs:
         raw1_events = mne.find_events(raw)
         raw2_events = mne.find_events(raw2)
         raw1_blocks = set(raw1_events[:, -1])
         raw2_blocks = set(raw2_events[:, -1])
         if len(raw1_blocks & raw2_blocks) == 3:  # common block in [1, 4, 8]
             common_block = [1, 4, 8][len(raw1_blocks) // 4]
+            if common_block in (4, 8):
+                prev_block = common_block - 1
+                prev_block_len = mdf[(mdf['subj'] == subj) &
+                                     (mdf['block'] == prev_block)].shape[0]
+                common_block_ix = (np.where(raw1_events[:, -1] == prev_block
+                                            )[0] + prev_block_len + 1)
+            else:
+                raise RuntimeError('You\'re throwing away the entire first '
+                                   'EEG run. Why not rename the input files '
+                                   'so the unusable one never gets read in.')
         else:
             common_block = (raw1_blocks & raw2_blocks) - set([1, 4, 8])
             assert len(common_block) == 1
             common_block = common_block.pop()
-        common_block_ix = np.where(raw1_events[:, -1] == common_block)[0]
-        assert common_block_ix.size == 1
+            common_block_ix = np.where(raw1_events[:, -1] == common_block)[0]
+            assert common_block_ix.size == 1
         first_raw2_ix = raw1_events.shape[0] + 1
+        # concatenate raws, then purge events from the partial repeated block
         raw = mne.concatenate_raws([raw, raw2])
         raw_events = mne.find_events(raw)
         raw_events = np.r_[raw_events[:common_block_ix[0]],
                            raw_events[first_raw2_ix:]]
         del (h, raw2, raw1_events, raw2_events, common_block, common_block_ix,
              first_raw2_ix)
-    except IOError:
-        pass
+    # picks
     picks = mne.pick_types(raw.info, meg=False, eeg=True, eog=False,
                            stim=False, exclude='bads')
     # decode triggers to get proper event codes
@@ -129,20 +154,23 @@ for subj_code, subj in subjects.items():
     wav_dur_by_trial = np.array([wav_dur_dict[rev_ev_id[_id]]
                                  for _id in events[:, -1]])
     # prepare time-shifted event codes aligned on C-V boundary
-    orig_secs = np.squeeze([raw.index_as_time(samp) for samp in events[:, 0]])
+    assert raw.first_samp == 0
+    orig_secs = np.squeeze([raw.times[samp] for samp in events[:, 0]])
     events_cv = events.copy()
     events_cv[:, 0] = raw.time_as_index(orig_secs + consonant_dur_by_trial)
     mne.write_events(basename + 'v-aligned-eve.txt', events_cv)
-    # filter, could be: l_freq=0.5, l_trans_bandwidth=0.4
-    raw.filter(l_freq=1, h_freq=40., n_jobs='cuda', copy=False)
-    # generate epochs aligned on C onset
+    # baseline, reference, and filter
     baseline = baseline_times if do_baseline else None
     mne.io.set_eeg_reference(raw, ref_channels=['Ch17'], copy=False)
+    raw.filter(l_freq=1, h_freq=40., n_jobs='cuda', copy=False)
+    # generate epochs aligned on C onset
     epochs = mne.Epochs(raw, events, event_id, tmin, tmax, picks=picks,
                         add_eeg_ref=True, baseline=baseline, preload=True)
+    epochs = epochs.resample(100, npad=0, n_jobs='cuda')
     # generate epochs aligned on C-V transition
     epochs_cv = mne.Epochs(raw, events_cv, event_id, tmin_cv, tmax_cv,
                            picks=picks, baseline=None, preload=True)
+    epochs_cv = epochs_cv.resample(100, npad=0, n_jobs='cuda')
     # zero-out EEG responses irrelevant to the current stimulus (ideally
     # not necessary, but maybe helpful due to short ISIs)
     for e_ix, e in enumerate((epochs_cv, epochs)):
@@ -166,14 +194,19 @@ for subj_code, subj in subjects.items():
             # zero out the early/late parts
             e._data[ix, :, :c_start_ix] = 0.
             e._data[ix, :, v_end_ix:] = 0.
-        # compute DSS matrix
-        dss_mat = dss(e, data_thresh=1e-3, bias_thresh=1e-3, return_data=False)
+        # compute DSS matrix. Keep all non-zero components for now.
+        dss_mat, dss_data = dss(e, data_thresh=None, bias_thresh=None)
         align = 'v' if e_ix else 'c'
-        np.save(basename + '{}-aligned-dssmat.npy'.format(align), dss_mat,
-                allow_pickle=False)
+        if save_dss_mat:
+            np.save(basename + '{}-aligned-dssmat.npy'.format(align),
+                    dss_mat, allow_pickle=False)
+        if save_dss_data:
+            np.save(basename + '{}-aligned-dssdata.npy'.format(align),
+                    dss_data, allow_pickle=False)
     # save epochs
-    epochs.save(basename + 'c-aligned-epo.fif.gz')
-    epochs_cv.save(basename + 'v-aligned-epo.fif.gz')
+    if save_epochs:
+        epochs.save(basename + 'c-aligned-epo.fif.gz')
+        epochs_cv.save(basename + 'v-aligned-epo.fif.gz')
 
 # finish
 np.savez(op.join(paramdir, 'subjects.npz'), **subjects)
