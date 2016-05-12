fb88a3937fb6fa9279c006d91332f6892f81fc0c
drammock <dan.mccloy@gmail.com>
2016-05-12 14:55:28 -0700

    add more plotting files

---
+++ plot-EER.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+"""
+===============================================================================
+Script 'plot-EER.py'
+===============================================================================
+
+This script plots equal error rate as a grouped barplot.
+"""
+# @author: drmccloy
+# Created on Wed Apr  6 12:43:04 2016
+# License: BSD (3-clause)
+
+
+from __future__ import division, print_function
+import numpy as np
+import os.path as op
+from pandas import read_csv
+from expyfun.analyze import barplot
+import matplotlib.pyplot as plt
+plt.ioff()
+
+# flags
+savefig = True
+
+# file I/O
+figdir = 'figures'
+paramdir = 'params'
+outdir = 'processed-data'
+
+# styles
+colors = [str(x) for x in np.linspace(0.2, 0.8, 5)]
+plt.rc('font', family='sans-serif', size=8, **{'sans-serif': 'M+ 1c'})
+
+# load data
+foreign_langs = np.load(op.join(paramdir, 'foreign-langs.npy'))
+eer = read_csv(op.join(outdir, 'equal-error-rates.tsv'), sep='\t', index_col=0)
+# sort English first
+eer = eer[['eng', 'hun', 'nld', 'swh', 'hin']]
+# axis labels
+bars = eer.values.ravel()
+groups = np.arange(bars.size).reshape(-1, eer.columns.size)
+group_names = dict(consonantal='conson.', sonorant='sonor.',
+                   continuant='contin.',
+                   delayedRelease='del. rel.', approximant='approx.',
+                   nasal='nasal', lateral='lateral', labial='labial',
+                   labiodental='labiodent.', coronal='coronal',
+                   anterior='anterior', distributed='distrib.',
+                   strident='strid.', dorsal='dorsal',
+                   spreadGlottis='aspir.', periodicGlottalSource='voiced')
+gn = [group_names[x] for x in eer.index]
+# bar_names = dict(hin='H', swh='S', hun='U', nld='D', eng='E')
+# bn = [bar_names[x] for x in eer.columns] * eer.shape[0]
+
+fig, ax = plt.subplots(1, 1, figsize=(9, 5))
+ax, bar = barplot(bars, groups=groups, ax=ax, group_names=gn, bar_names=' ',
+                  ylim=(0, 0.55), gap_size=0.6, bar_kwargs=dict(color=colors))
+ax.axhline(0.5, color='k', linestyle=(0, (5, 7)), linewidth=1)
+ax.set_ylabel('Equal error rate')
+ax.legend(bar.get_children()[:5], (eer.columns), loc='upper right')
+plt.tight_layout()
+if savefig:
+    plt.savefig(op.join(figdir, 'eer-barplot.pdf'))
+else:
+    plt.ion()
+    plt.show()
---
+++ plot-feature-based-confusion-matrices.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+"""
+===============================================================================
+Script 'plot-feature-based-confusion-matrices.py'
+===============================================================================
+
+This script plots confusion matrices.
+"""
+# @author: drmccloy
+# Created on Wed Apr  6 12:43:04 2016
+# License: BSD (3-clause)
+
+from __future__ import division, print_function
+import numpy as np
+from os import path as op
+from pandas import read_csv
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import ImageGrid
+plt.ioff()
+
+# flags
+savefig = True
+negative_log = True
+figsize = (4, 24)
+
+# file I/O
+figdir = 'figures'
+paramdir = 'params'
+
+# load list of languages
+foreign_langs = np.load(op.join(paramdir, 'foreign-langs.npy'))
+lang_names = dict(hin='Hindi', swh='Swahili', hun='Hungarian', nld='Dutch',
+                  eng='English')
+
+# set up plot
+labelsize = 8
+colormap = 'viridis_r' if negative_log else 'viridis'
+plt.rc('font', serif='Charis SIL', family='serif', size=12)
+plt.rc('axes.spines', top=False, right=False, left=False, bottom=False)
+plt.rc('xtick.major', size=10, pad=2, width=0.25)
+plt.rc('xtick.minor', size=0, pad=2)
+plt.rc('ytick.major', size=12, pad=2, width=0.25)
+plt.rc('ytick.minor', size=0, pad=2)
+plt.rc('ytick', right=False)
+plt.rc('xtick', top=False)
+# initialize figure
+fig = plt.figure(figsize=figsize)
+axs = ImageGrid(fig, 111, nrows_ncols=(len(foreign_langs), 1),
+                axes_pad=0.5, label_mode='all')
+
+# iterate over languages
+for ix, lang in enumerate(foreign_langs):
+    fpath = op.join(paramdir, 'features-confusion-matrix-{}.tsv').format(lang)
+    featmatch = read_csv(fpath, sep='\t', encoding='utf-8', index_col=0)
+    confmat = -np.log2(featmatch.T) if negative_log else featmatch.T
+    ax = axs[ix]
+    ax.imshow(confmat, cmap=plt.get_cmap(colormap))
+    ax.set_ylabel(lang_names[lang])
+    ax.set_xticks(np.arange(confmat.shape[1])[1::2], minor=False)
+    ax.set_xticks(np.arange(confmat.shape[1])[::2], minor=True)
+    ax.set_yticks(np.arange(confmat.shape[0])[1::2], minor=False)
+    ax.set_yticks(np.arange(confmat.shape[0])[::2], minor=True)
+    ax.set_xticklabels(confmat.columns[1::2], minor=False, size=labelsize)
+    ax.set_xticklabels(confmat.columns[::2], minor=True, size=labelsize)
+    ax.set_yticklabels(confmat.index[1::2], minor=False, size=labelsize)
+    ax.set_yticklabels(confmat.index[::2], minor=True, size=labelsize)
+    if ix == len(foreign_langs) - 1:
+        ax.set_xlabel('English')
+
+# finish
+if savefig:
+    plt.savefig(op.join(figdir, 'feature-confusion-matrices.pdf'))
+else:
+    plt.ion()
+    plt.show()
