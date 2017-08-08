852644ab6768fc78e242f78a4d25e3ca3b6dd30e
drammock <dan.mccloy@gmail.com>
2017-08-08 16:00:37 -0700

    split off time-domain dimensionality reduction script

---
+++ 037-time-domain-redux.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+===============================================================================
+Script 'time-domain-redux.py'
+===============================================================================
+
+This script runs PCA on the time domain of epoched data, to reduce colinearity
+of "features" (AKA time points) prior to classification, and then unrolls the
+EEG channels (or DSS components, if using DSS) before saving.
+"""
+# @author: drmccloy
+# Created on Tue Aug  8 13:33:19 PDT 2017
+# License: BSD (3-clause)
+
+import yaml
+import mne
+import numpy as np
+import os.path as op
+from os import mkdir
+from time import time
+from aux_functions import time_domain_pca, print_elapsed
+
+np.set_printoptions(precision=6, linewidth=160)
+
+# BASIC FILE I/O
+indir = 'eeg-data-clean'
+outdir = op.join(indir, 'time-domain-redux')
+paramdir = 'params'
+if not op.isdir(outdir):
+    mkdir(outdir)
+
+# LOAD PARAMS FROM YAML
+analysis_param_file = 'current-analysis-settings.yaml'
+with open(op.join(paramdir, analysis_param_file), 'r') as f:
+    analysis_params = yaml.load(f)
+    subjects = analysis_params['subjects']
+    align_on_cv = analysis_params['align_on_cv']
+    do_dss = analysis_params['dss']['use']
+    n_comp = analysis_params['dss']['n_components']
+    pca_truncate = analysis_params['pca']['truncate_to_n_timepts']
+    skip = analysis_params['skip']
+
+# file naming variables
+cv = 'cvalign-' if align_on_cv else ''
+
+# iterate over subjects
+for subj_code, subj in subjects.items():
+    if subj_code in skip:
+        continue
+    basename = '{0:03}-{1}-{2}'.format(subj, subj_code, cv)
+    # load data
+    print('loading data for subject {}'.format(subj_code), end=': ')
+    _st = time()
+    epochs = mne.read_epochs(op.join(indir, 'epochs', basename + 'epo.fif.gz'),
+                             verbose=False)
+    if do_dss:
+        data = np.load(op.join(indir, 'dss', basename + 'dss-data.npy'))
+        data = data[:n_comp]
+    else:
+        data = epochs.get_data()
+    print_elapsed(_st)
+
+    # reduce time-domain dimensionality
+    print('running PCA on time domain', end=': ')
+    _st = time()
+    data = time_domain_pca(data, max_components=pca_truncate)
+    print_elapsed(_st)
+
+    # unroll data / concatenate channels
+    data = data.reshape(data.shape[0], -1)  # (n_epochs, n_chans * n_times)
+
+    # save
+    file_ext = 'dss{}-data.npy'.format(n_comp) if do_dss else 'epoch-data.npy'
+    out_fname = basename + 'redux-' + file_ext
+    np.save(op.join(outdir, out_fname), data, allow_pickle=False)
