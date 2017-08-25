07a67387e0ac58ee1e865dd7b361e99223001896
drammock <dan.mccloy@gmail.com>
2017-08-25 12:44:26 -0700

    add htop; chown mountpoint; comments

--- aws/ec2-setup.sh
+++ aws/ec2-setup.sh
@@ -1,16 +1,23 @@
 #!/bin/sh
 
+# install htop
+sudo apt install htop
+
+# mount EBS drive
 sudo mkfs -t ext4 /dev/xvdf
 MOUNTPOINT="$HOME/ebsdrive"
 mkdir "$MOUNTPOINT"
 sudo mount /dev/xvdf "$MOUNTPOINT"
+sudo chown ubuntu:ubuntu $MOUNTPOINT
 
-mkdir "$MOUNTPOINT/eeg_phone_coding"
-
+# install miniconda
 PREFIX="$MOUNTPOINT/miniconda"
 MINICONDA_PATH="$PREFIX/bin"
 curl https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -o miniconda.sh
-sh miniconda.sh -b -p $PREFIX
+bash miniconda.sh -b -p $PREFIX
 export PATH="$MINICONDA_PATH:$PATH"
 conda env create -f params/conda-env.yaml
 source activate eeg_phone_coding
+
+# make target directory for rsync
+mkdir "$MOUNTPOINT/eeg_phone_coding"
